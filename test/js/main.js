69bde90c5fe14e5c424cdd3e216617ea96e9a3a4
PabloMGN <pablo.mgnc@gmail.com>
2017-08-05 11:52:41 +0200

    Updated works
    More or less final works

--- test/js/main.js
+++ test/js/main.js
@@ -35,3 +35,19 @@ $('.filter-button-group').on( 'click', 'button', function() {
   var filterValue = $(this).attr('data-filter');
   $grid.isotope({ filter: filterValue });
 });
+
+$(document).ready(function(){       
+   var scroll_start = 0;
+   var startchange = $('#startchange');
+   var offset = startchange.offset();
+    if (startchange.length){
+   $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $(".navbar-inverse").css('background-color', '#f0f0f0');
+       } else {
+          $('.navbar-inverse').css('background-color', 'transparent');
+       }
+   });
+    }
+});
